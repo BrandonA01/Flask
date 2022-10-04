4f127c0ecbc2fd67b12582e3f5ca3e7f5f10ed2e
Brandon Allen <81233247+BrandonA01@users.noreply.github.com>
2022-10-04 12:00:15 +0100

    New Commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from ssl import AlertDescription
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
@@ -6,15 +7,26 @@ import http.client
 import requests
 
 url = "https://online-movie-database.p.rapidapi.com/title/v2/find"
+querystring = {"title":"","titleType":"movie","limit":"5","paginationKey":"0","sortArg":"moviemeter,asc"}
 
-querystring = {"title":"matrix","titleType":"movie","limit":"6","paginationKey":"0","sortArg":"moviemeter,asc"}
+urlPopular = "https://online-movie-database.p.rapidapi.com/title/get-most-popular-movies"
+querystringPopular = {"currentCountry":"GB","purchaseCountry":"GB","homeCountry":"GB"}
+
+urlPopDet = "https://online-movie-database.p.rapidapi.com/title/get-details"
+querystringPopDet = {"tconst":""}
+
+urlDetails = "https://online-movie-database.p.rapidapi.com/title/get-overview-details"
+querystringDetails = {"tconst":"","currentCountry":"US"}
 
 headers = {
-	"X-RapidAPI-Key": "f8441a6a6bmsh72724c87376c9a4p163c8cjsnf48b2a3445ef",
+	"X-RapidAPI-Key": "",
 	"X-RapidAPI-Host": "online-movie-database.p.rapidapi.com"
 }
 
 response = requests.request("GET", url, headers=headers, params=querystring)
+responsePopular = requests.request("GET", urlPopular, headers=headers, params=querystringPopular)
+responsePopularDetails = requests.request("GET", urlPopDet, headers=headers, params=querystringPopDet)
+responseMoreDetails = requests.request("GET", urlDetails, headers=headers, params=querystringDetails)
 
 
 app = Flask(__name__)
@@ -24,26 +36,21 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flaskDB.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.permanent_session_lifetime = timedelta(days=5)
 
-"""
-conn = http.client.HTTPSConnection("online-movie-database.p.rapidapi.com")
-headers = {
-    'X-RapidAPI-Key': "f8441a6a6bmsh72724c87376c9a4p163c8cjsnf48b2a3445ef",
-    'X-RapidAPI-Host': "online-movie-database.p.rapidapi.com"
-    }
-"""
-
 db = SQLAlchemy(app)
 
 class users(db.Model):
-    _id = db.Column("id", db.Integer, primary_key=True)
+    email = db.Column(db.String(100), primary_key=True)
     firstName = db.Column(db.String(32))
     lastName = db.Column(db.String(32))
-    email = db.Column(db.String(100))
+    password = db.Column(db.Integer)
+    favourites = db.Column(db.String)
 
-    def __init__(self, firstName, lastName, email):
+    def __init__(self, email, firstName, lastName, password, favourites):
         self.firstName = firstName
         self.lastName = lastName
         self.email = email
+        self.password = password
+        self.favourites = favourites
 
 class films(db.Model):
     _id = db.Column("filmId", db.Integer, primary_key=True)
@@ -56,6 +63,37 @@ class films(db.Model):
         self.description = description
         self.rating = rating
 
+class popularFilms(db.Model):
+    _id = db.Column("id", db.String(10), primary_key=True)
+    title = db.Column(db.String(64))
+    url = db.Column(db.String(256))
+
+    def __init__(self, _id, title, url):
+        self._id = _id
+        self.title = title
+        self.url = url
+
+@app.route('/search', methods=['POST'])
+def search():
+        search = request.form["search"]
+        if search != "":
+            search = search.strip
+            print(search)
+            return render_template("search.html", length=1, values=search)
+        else:
+            return render_template("index.html", values=popularFilms.query.all(), length=len(popularFilms.query.all()))
+    
+@app.route("/film/<id>")
+def film(id):
+    querystringDetails["tconst"] = id
+    responseMoreDetails = json.loads(requests.request("GET", urlDetails, headers=headers, params=querystringDetails).text)
+    print(responseMoreDetails)
+    if "rating" in responseMoreDetails["ratings"]:
+        rating = responseMoreDetails["ratings"]["rating"]
+    else:
+        rating = 0
+    return render_template("film.html", title=responseMoreDetails["title"]["title"], url=responseMoreDetails["title"]["image"]["url"], desc=responseMoreDetails["plotOutline"]["text"], rat=rating)
+
 @app.route("/viewUsers")
 def view():
     return render_template("view.html", values=users.query.all())
@@ -64,10 +102,32 @@ def view():
 
 @app.route("/")
 def home():
-    return render_template("index.html", values=json.loads(response.text)["results"], length=len(json.loads(response.text)["results"]))
-    #return render_template("index.html", values=films.query.all())
-    
-    """
+    jsonList = {"films": []}
+    return render_template("index.html", values=popularFilms.query.all(), length=len(popularFilms.query.all()))
+    #return z
+"""
+    limit = 100
+    for index, item in enumerate(json.loads(responsePopular.text)):
+        if index == limit:
+            break
+        item = item.replace("/title/", "")
+        item = item.replace("/", "")
+
+
+        x = json.dumps(jsonList)
+        z = json.loads(x)
+        querystringPopDet["tconst"] = item
+        responsePopularDetails = requests.request("GET", urlPopDet, headers=headers, params=querystringPopDet)
+        
+        print(json.loads(responsePopularDetails.text))
+        
+        popFilm = popularFilms(item, json.loads(responsePopularDetails.text)["title"], json.loads(responsePopularDetails.text)["image"]["url"])
+        db.session.add(popFilm)
+        db.session.commit()
+
+        z["films"].append(responsePopularDetails.text)
+        jsonList = z
+
     conn.request("GET", "/auto-complete?q=game%20of%20thr", headers=headers)
     res = conn.getresponse()
     data = res.read()
@@ -75,22 +135,24 @@ def home():
     return json.dumps(data, default=str)
     """
 
-@app.route("/user/", methods=["POST","GET"])
+@app.route("/user", methods=["POST","GET"])
 def user():
-    email = None
-    if "user" in session:
-        user = session["user"]
+    if "email" in session:
+        curremail = session["email"]
         if request.method == "POST":
-            email = request.form["email"]
-            session["email"] = email
-            found_user = users.query.filter_by(firstName=user).first()
-            found_user.email = email
+            emailInput = request.form["email"]
+            fname = request.form["fname"]
+            lname = request.form["lname"]
+            session["email"] = emailInput
+            session["fname"] = fname
+            session["lname"] = lname
+            found_user = users.query.filter_by(email=curremail).first()
+            found_user.email = emailInput
+            found_user.firstName = fname
+            found_user.lastName = lname
             db.session.commit()
-            flash("Email was saved!", "info")
-        else:
-            if "email" in session:
-                email = session["email"]
-        return render_template("user.html", user=user, email=email)
+            flash("Details were saved!", "info")
+        return render_template("user.html", fname=session["fname"], lname=session["lname"], email=session["email"])
     else:
         flash("You are not logged in.", "info")
         return redirect(url_for("login"))
@@ -106,13 +168,14 @@ def admin():
 
 @app.route("/delete", methods=["POST", "GET"])
 def delete():
-    if "user" in session:
-        user = session["user"] 
-        users.query.filter_by(firstName=user).delete()
+    if "email" in session:
+        email = session["email"] 
+        users.query.filter_by(email=email).delete()
         db.session.commit()
-        session.pop("user", None)
         session.pop("email", None)
-        flash("Shit just got real")
+        session.pop("fname", None)
+        session.pop("lname", None)
+        flash("Deleted Account!")
         return redirect(url_for("login"))
     else:
         flash("you must login to delete an account")
@@ -122,25 +185,33 @@ def delete():
 @app.route("/login/", methods=["POST","GET"])
 def login():
     if request.method == "POST":
-        user = request.form["nm"]
-        if len(user) != 0:
+        email = request.form["logemail"]
+        password = request.form["logpass"]
+        if len(email) != 0 and len(password) != 0:
             session.permanent = True
-            session["user"] = user            
-            found_user = users.query.filter_by(firstName=user).first()
+            session["email"] = email
+            found_user = users.query.filter_by(email=email).first()
             if found_user:
-                session["email"] = found_user.email
-
+                if password == found_user.password:
+                    session["email"] = found_user.email
+                    session["fname"] = found_user.firstName
+                    session["lname"] = found_user.lastName
+                    flash("Logged in successfully.", "info")
+                    return redirect(url_for("user"))
+                else:
+                    flash("email or password is incorrect", "info")
+                    return render_template("login.html")
             else:
-                usr = users(user, None, None)
+                usr = users(email, None, None, password)
                 db.session.add(usr)
                 db.session.commit()
+                flash("Created account.", "info")
+                return redirect(url_for("user"))
 
-            flash("Logged in successfully.", "info")
-            return redirect(url_for("user"))
         else:
             return render_template("login.html")
     else:
-        if "user" in session:
+        if "email" in session:
             flash("Already Logged In!", "info")
             return redirect(url_for("user"))
         else:
@@ -149,17 +220,16 @@ def login():
 
 @app.route("/logout")
 def logout():
-    if "user" in session:
-        session.pop("user", None)
+    if "email" in session:
         session.pop("email", None)
+        session.pop("fname", None)
+        session.pop("lname", None)
         flash("You have logged out successfully", "info")
         return redirect(url_for("login"))
     else:
         flash("You are currently not logged in", "info")
         return redirect(url_for("login"))
     
-
-
 
 if __name__ == "__main__":
     db.create_all()
